389670be3f269fbd280c1ce5a611b0538a07ced5
Victor Ebube Omekwu <71439336+ebubevick@users.noreply.github.com>
2024-08-16 07:06:19 -0500

    first commit

--- prerequisitie-install/tools.sh
+++ prerequisitie-install/tools.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exit script on any error
-# set -e
-
 echo "Installing prerequisites..."
 
 # Update system packages
@@ -13,27 +10,26 @@ sudo apt-get install -y curl
 
 # Install Docker
 echo "Installing Docker..."
-sudo apt-get update -y &&
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common &&
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
-sudo apt-get update -y &&
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y &&
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 newgrp docker
 
 # Install Helm
 echo "Installing Helm..."
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get install -y apt-transport-https
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list > /dev/null
 sudo apt-get update
-sudo apt-get install helm -y
+sudo apt-get install -y helm
 
 # Install Azure CLI
 echo "Installing Azure CLI..."
@@ -46,7 +42,7 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 # Install Kubectl
 echo "Installing Kubectl..."
-curl -LO "https://dl.k8s.io/release/v$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 

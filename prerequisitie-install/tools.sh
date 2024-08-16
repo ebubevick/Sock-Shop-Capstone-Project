59c24ca72317fd40b86052ae185f46f824fa8720
Victor Ebube Omekwu <71439336+ebubevick@users.noreply.github.com>
2024-08-16 07:16:24 -0500

    first commit

--- prerequisitie-install/tools.sh
+++ prerequisitie-install/tools.sh
@@ -40,6 +40,10 @@ echo "Installing Minikube..."
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+# Start Minikube
+echo "Starting Minikube..."
+minikube start
+
 # Install Kubectl
 echo "Installing Kubectl..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
